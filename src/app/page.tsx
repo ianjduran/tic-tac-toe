db1d3ff494be095d106c34bf43bd3ed6c4c843f5
Ian Duran <ianjduran@gmail.com>
2024-03-28 11:59:27 -0600

    fix font theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Board from "./components/Board";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center  p-12 bg-gradient-to-br from-gray-950 via-slate-950 via-55% to-sky-950"> 
+    <main className="flex min-h-screen flex-col items-center  p-12 bg-gradient-to-br from-gray-950 via-slate-950 via-55% to-sky-950 text-white"> 
     {/* justify-between */}
         <div className="flex flex-col items-center mb-8">
             <h1 className="font-bold text-3xl">Tic-Tac-Toe</h1>
